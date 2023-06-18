0d9ed7d7e134a22d36236e8f79128a07fdc038be
Krzysztof Adamek <kadamek@alx.pl>
2023-06-18 15:48:52 +0200

    Zmieniono implementacje sumowania na sume ciagu arytmetycznego

--- suma.cpp
+++ suma.cpp
@@ -3,8 +3,6 @@
 int main() {
     int n;
     std::cin >> n;
-    int suma = 0;
-    for (int i = 1; i <= n; i += 1)
-        suma += i;
+    int suma = n * (n + 1);
     std::cout << "Suma pierwszych " << n << " liczb naturalnych to " << suma << '\n';
 }
