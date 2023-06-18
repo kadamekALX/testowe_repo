d0fc47a11fd5b6d4b4e5c86b54b016496e69f03b
Krzysztof Adamek <kadamek@alx.pl>
2023-06-18 15:58:06 +0200

    Naprawiono blad we wzorze na sume

--- suma.cpp
+++ suma.cpp
@@ -3,6 +3,6 @@
 int main() {
     int n;
     std::cin >> n;
-    int suma = n * (n + 1);
+    int suma = n * (n + 1) / 2;
     std::cout << "Suma pierwszych " << n << " liczb naturalnych to " << suma << '\n';
 }
